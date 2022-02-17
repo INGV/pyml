f4a7c83a6f72f6be8127b6d9a504d16f82c2c535
Raffaele <raffaele@arkham.int.ingv.it>
2022-02-17 17:06:56 +0100

    bug on lambda

--- pyml.py
+++ pyml.py
@@ -168,7 +168,7 @@ def calculate_event_ml(magnitudes,magnitudes_sta,maxit,stop,max_dev,hm_cutoff):
           Ml_Medi_old = Ml_Medi
           distance_from_mean = abs(m - Ml_Medi)
           if hm_cutoff:
-             w = numpy.asfarray(list(filter(lambda x: 1.0 if (x <= cutoff) else hm_cutoff/x,m))) # Values beyond cutoff are downweighted
+             w = numpy.asfarray(list(filter(lambda x: 1.0 if (x <= hm_cutoff) else hm_cutoff/x,m))) # Values beyond cutoff are downweighted
              print(w)
           not_outlier = distance_from_mean < max_dev * Ml_Std
           yes_outlier = distance_from_mean >= max_dev * Ml_Std
