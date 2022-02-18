6db57f5f74d58616dc1d25d5797a6ef932bea726
Raffaele <raffaele@Arkham.local>
2022-02-18 13:22:10 +0100

    testing the weight and bug fix

--- pyml.py
+++ pyml.py
@@ -165,9 +165,9 @@ def calculate_event_ml(magnitudes,magnitudes_sta,maxit,stop,max_dev,hm_cutoff):
           Ml_Medi_old = Ml_Medi
           distance_from_mean = abs(m - Ml_Medi)
           if hm_cutoff:
-             w = filter(lambda x: 1.0 if x <= hm_cutoff else hm_cutoff/x,distance_from_mean) # Values beyond cutoff are downweighted
+             wf = lambda x: 1.0 if x <= hm_cutoff else hm_cutoff/x # Values beyond cutoff are downweighted
              print("Stampeso su Media",str(Ml_Medi))
-             print(distance_from_mean)
+             w = wf(distance_from_mean)
              print(w)
              Ml_Medi = numpy.sum(m * w)/numpy.sum(w)
              deltaMean = abs(Ml_Medi-Ml_Medi_old)
