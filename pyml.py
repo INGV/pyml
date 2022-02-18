64f20343a7b28e8724a8cdb82716b33e0a09a4bd
Raffaele <raffaele@Arkham.local>
2022-02-18 18:59:56 +0100

    check on pseudo-zero weights with numpy stack

--- pyml.py
+++ pyml.py
@@ -173,6 +173,7 @@ def wstd(v,wm,wf):
        wsd = False
     else:
        stack = numpy.stack(wf,v)
+       print(stack)
        wsd = numpy.sum(numpy.where(stack[:,0] > eps2,stack[:,0]*numpy.square((stack[:,1]-wm))))
        #wsd = numpy.sum(wf*numpy.square((v-wm)))
        wf2 = numpy.where(wf > eps2)
